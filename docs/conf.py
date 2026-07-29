6d333eacb9a9c6bcfd28f6a6874e00f34c840347
taxguide <singhparash06@gmail.com>
2026-07-29 17:05:19 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,21 @@
+# Configuration file for the Sphinx documentation builder.
 
+project = 'hulu.com/activate'
+author = 'hulu.com/activate'
+release = '1.0'
+
+extensions = []
+
+# Templates
+templates_path = ['_templates']
+
+exclude_patterns = []
+
+html_theme = 'alabaster'
+
+# Static files
+html_static_path = ['_static']
+
+language = 'en'
+
+html_title = "hulu.com/activate"
